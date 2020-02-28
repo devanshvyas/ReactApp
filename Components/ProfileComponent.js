ea6b1ff5ad1f4d4a3c4871e23e43169a2f5349e6
Devansh Vyas <devansh.vyas@solutionanalysts.com>
2020-02-28 18:57:39 +0530

    [WIP] profile screen

--- Components/ProfileComponent.js
+++ Components/ProfileComponent.js
@@ -1,12 +1,17 @@
 import React, { Component } from "react";
-import { SafeAreaView, Image, Text } from "react-native";
+import { SafeAreaView, Image, Text, StyleSheet, View } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 
 export default class ProfileComponent extends Component {
     render() {
-        return <SafeAreaView>
-            <Image source={require('../assets/avatar.png')}></Image>
-            
+        return <SafeAreaView style={styles.main}>
+            <View style={styles.imageContainer}>
+            <Image source={require('../assets/avatar.png')} style={styles.image}></Image>
+            <TouchableOpacity>
+                <Image source={require('../assets/edit.png')}/>
+            </TouchableOpacity>
+            </View>
             <Text>Name</Text>
             <Text>Email</Text>
 
@@ -14,3 +19,19 @@ export default class ProfileComponent extends Component {
     }
 }
 
+const styles = StyleSheet.create({
+    main: {
+        flex: 1,
+        flexDirection:'column'
+    },
+    imageContainer: {
+        flex: 0.3,
+        alignItems:'center',
+        backgroundColor: 'red'
+    },
+    image: {
+        top:10,
+        height: 100,
+        width: 100,
+    }
+})
